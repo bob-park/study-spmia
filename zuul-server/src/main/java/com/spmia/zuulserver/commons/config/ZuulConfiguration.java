27d7f7ab3b9adef629fd6c975e1b745abd17ead0
phw11 <hwpark11@kakao.com>
2021-09-12 23:21:28 +0900

    feat : spring cloud gateway 적용 및 설정

--- zuul-server/src/main/java/com/spmia/zuulserver/commons/config/ZuulConfiguration.java
+++ zuul-server/src/main/java/com/spmia/zuulserver/commons/config/ZuulConfiguration.java
@@ -1,11 +1,8 @@
 package com.spmia.zuulserver.commons.config;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableDiscoveryClient
-// * @EnableZuulServer 도 있지만 이것은 Eureka 가 아닌 다른 service discovery engine 과 통합할 경우 사용한다.
 public class ZuulConfiguration {
 //
 //  @LoadBalanced
