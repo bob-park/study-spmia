8e9585fda62d46c6826274007568693a41fcd467
phw11 <hwpark11@kakao.com>
2021-09-12 13:47:19 +0900

    feat : actuator 설정 추가

--- zuul-server/src/main/java/com/spmia/zuulserver/commons/config/ZuulConfiguration.java
+++ zuul-server/src/main/java/com/spmia/zuulserver/commons/config/ZuulConfiguration.java
@@ -6,5 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 // * @EnableZuulServer 도 있지만 이것은 Eureka 가 아닌 다른 service discovery engine 과 통합할 경우 사용한다.
 @EnableZuulProxy // service 를 zuul server 로 사용한다.
-public class ZuulConfiguration {
-}
+public class ZuulConfiguration {}
