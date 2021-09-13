923f6c09eed4cff15f67bd8cc46beff08fd889da
phw11 <hwpark11@kakao.com>
2021-09-13 21:48:23 +0900

    feat : special route service 추가

---
+++ specialroute-service/src/main/java/com/spmia/specialrouteservice/SpecialrouteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spmia.specialrouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialrouteServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpecialrouteServiceApplication.class, args);
+  }
+}
---
+++ specialroute-service/src/test/java/com/spmia/specialrouteservice/SpecialrouteServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.spmia.specialrouteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialrouteServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
